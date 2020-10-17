f4c21e3463640e3f43cccbf0bbcf6d3a6dbe7a7f
Anisha Banik <anisha@codeiam.com>
2020-10-17 12:19:13 +0530

    first commit

---
+++ Cartesian/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        char[] chars = { 'X', 'Y', 'Z' };
+        int[] num = { 1, 2, 3 };
+        string[] color = { "Orange", "Red" };
+
+
+
+        var Prod = from lett in chars
+                   from numb in num
+                   from col in color
+                   select new { lett, numb, col };
+
+        Console.Write("The Product sets are : \n");
+        foreach (var prod1 in Prod)
+        {
+            Console.WriteLine(prod1);
+        }
+
+    }
+}
